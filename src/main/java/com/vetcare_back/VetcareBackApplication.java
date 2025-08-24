1403e17e221ff19376e6c3670a92a64ea6d72022
YeisonAndresDP <andresyeison3004@gmail.com>
2025-08-24 11:33:36 -0500

    Initial project setup

---
+++ src/main/java/com/vetcare_back/VetcareBackApplication.java
@@ -0,0 +1,13 @@
+package com.vetcare_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetcareBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VetcareBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vetcare_back/VetcareBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vetcare_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetcareBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
